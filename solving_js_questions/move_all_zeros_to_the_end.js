72df9336fd66b6175a906709dcd6ced57c0aa13b
Vishal <vishalofficial224@gmail.com>
2026-04-12 18:06:17 +0530

    remove all zeroes from the end of an array

--- solving_js_questions/move_all_zeros_to_the_end.js
+++ solving_js_questions/move_all_zeros_to_the_end.js
@@ -1,24 +1,17 @@
 
 
-const apple = [0,1, 4, 5, 7, 8, 9];
+const apple = [0,0,0,0,0,0, 5, 7, 8, 9];
+
+
+const newArray = apple.filter(x => x !==0);
+
+const howManyZeros = apple.filter(x => x === 0);
 
 
 
-let newArray1 = [];
-let newArray2 = [];
+for (let i in howManyZeros) {
 
-function newArray(value) {
-    
-    for (let i in apple) {
-        if (apple[i] === value) {
-            newArray1 = apple.slice(0, apple.indexOf(apple[i]))
-            newArray2 = apple.slice(apple.indexOf(apple[i])+1)
-        }
-    }
+    newArray.push(0)
 }
 
-
-newArray(9)
-
-const newArray3 = newArray1.concat(newArray2)
-
+console.log(newArray)
